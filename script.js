8e9beb13968e536577abbe63cc5abb3efc405499
BonsCaglar <caglarbardak19@gmail.com>
2022-05-30 00:24:47 +0300

    To Do App Finished

--- script.js
+++ script.js
@@ -3,27 +3,47 @@ let freeContainer = document.querySelector("#listToContainer");
 let inputText = document.querySelector("#textName");
 let btnClear = document.querySelector("#buttonClear");
 
-
 btnAdd.addEventListener("click", addfunct)
 
-function addfunct() {
+function addfunct(e) {
 
 
     if (textName.value == "") {
-        alert("Bir değer giriniz..")
+        alert("Null values ​​cannot be inserted!")
     } else {
         let newText = document.createElement("li");
-        newText.className = "list-group-item list-group-item-secondary mt-2";
+        newText.className = "list-group-item list-group-item-secondary mt-2 d-flex justify-content-between";
+        newText.innerText = textName.value;
 
-        let iconBtn = document.createElement("button");
-        iconBtn.className = "btn btn-outline-primary float-right ";
-        newText.appendChild(iconBtn);
+        let a = document.createElement("a");
+        a.setAttribute = ("href", "#");
+        a.className = "icon";
+        a.innerHTML = "<i class='fa-solid fa-xmark'></i>";
+        newText.appendChild(a);
 
-        document.querySelector("#listToContainer").appendChild(newText)
-        newText.innerHTML = textName.value;
+
+        document.querySelector("#listToContainer").appendChild(newText);
         textName.value = "";
 
-        console.log(newText);
+
+
+
+
+        let dlt = document.querySelector("#listToContainer").addEventListener("click", deleteİtem);
+
+        function deleteİtem(e) {
+
+            let item = e.target;
+
+            console.log(e.target);
+
+            if (item.classList[0] === "fa-solid") {
+
+                let todo = item.parentElement.parentElement;
+                todo.remove();
+            }
+
+        }
 
         btnClear.addEventListener("click", clear)
         function clear() {
@@ -31,6 +51,10 @@ function addfunct() {
         }
     }
 
+    e.preventdefault();
 
 
 };
+
+
+
